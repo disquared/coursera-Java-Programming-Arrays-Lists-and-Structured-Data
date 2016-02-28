b7de98928e2ab8dedc7a9d209fb065d137707893
Di Di <ddi0168@gmail.com>
2016-02-28 03:18:29 -0500

    week 4: Breaking the Vigenère Cipher

--- src/main/week4/VigenereBreaker.java
+++ src/main/week4/VigenereBreaker.java
@@ -1,6 +1,9 @@
 package week4;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import edu.duke.*;
 
 public class VigenereBreaker {
@@ -26,8 +29,100 @@ public class VigenereBreaker {
         }
         return key;
     }
+    
+    public Set<String> readDictionary(FileResource fr) {
+        return StreamSupport.stream(fr.lines().spliterator(), false)
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+    }
+    
+    public int countWords(String message, Set<String> dict) {
+        int validWords = 0;
+        String[] words = message.split("\\W+");
+        for (String word : words) {
+            if (dict.contains(word.toLowerCase())) {
+                validWords++;
+            }
+        }
+        return validWords;
+    }
+    
+    public char mostCommonCharIn(Set<String> dict) {
+        int[] charCounts = new int[26];
+        for (String word : dict) {
+            for (int i = 0; i < word.length(); i++) {
+                char letter = word.charAt(i);
+                if (letter >= 'a' && letter <= 'z') {
+                    charCounts[letter - 'a']++;
+                }
+            }
+        }
+        char mostCommonChar = '0';
+        int mostCommonCount = 0;
+        for (int i = 0; i < 26; i++) {
+            if (charCounts[i] > mostCommonCount) {
+                mostCommonCount = charCounts[i];
+                mostCommonChar = (char) (i + 'a');
+            }
+        }
+        return mostCommonChar;
+    }
+    
+    public String breakForLanguage(String encrypted, Set<String> dict) {
+        int mostValidWords = 0;
+        int[] keyMostValidWords = {};
+        for (int i = 1; i <= 100; i++) {
+            char mostCommonChar = mostCommonCharIn(dict);
+            int[] key = tryKeyLength(encrypted, i, mostCommonChar);
+            VigenereCipher vc = new VigenereCipher(key);
+            String decrypted = vc.decrypt(encrypted);
+            int validWords = countWords(decrypted, dict);
+            if (validWords > mostValidWords) {
+                mostValidWords = validWords;
+                keyMostValidWords = key;
+            }
+        }
+        //System.out.println("Num valid words: " + mostValidWords);
+        //System.out.println("Key length: " + keyMostValidWords.length);
+        return new VigenereCipher(keyMostValidWords).decrypt(encrypted);
+    }
+    
+    public String breakForAllLanguages(String encrypted, Map<String, Set<String>> languages) {
+        int maxNumWords = 0;
+        String decryptedMaxWords = "";
+        String decryptedLanguage = "";
+        for (String language : languages.keySet()) {
+            Set<String> dict = languages.get(language);
+            String decrypted = breakForLanguage(encrypted, dict);
+            int numWords = countWords(decrypted, dict);
+            if (numWords > maxNumWords) {
+                maxNumWords = numWords;
+                decryptedMaxWords = decrypted;
+                decryptedLanguage = language;
+            }
+        }
+        System.out.println("maxNumWords: " + maxNumWords);
+        System.out.println("language: " + decryptedLanguage);
+        return decryptedMaxWords;
+    }
+    
+    public void breakVigenere() {
+        FileResource fr = new FileResource("data/vigenere/secretmessage3.txt");
+        String encrypted = fr.asString();
+        //System.out.println(Arrays.toString(dict.toArray()));
+        Map<String, Set<String>> languageToDictMap = new HashMap<>();
+        String[] languages = {"Danish", "Dutch", "English", "French",
+                              "German", "Italian", "Portuguese", "Spanish"};
+        for (String language : languages) {
+            FileResource frDict = new FileResource("data/vigenere/dictionaries/" + language);
+            Set<String> dict = readDictionary(frDict);
+            languageToDictMap.put(language, dict);
+        }
+        String decrypted = breakForAllLanguages(encrypted, languageToDictMap);
+        System.out.println(decrypted);
+    }
 
-    public void breakVigenere () {
+    public void oldBreakVigenere () {
         //WRITE YOUR CODE HERE
         FileResource resource = new FileResource("data/vigenere/secretmessage1.txt");
         String input = resource.asString();
@@ -40,7 +135,6 @@ public class VigenereBreaker {
     
     public static void main(String[] args) {
         VigenereBreaker vb = new VigenereBreaker();
-    	//System.out.println(vb.sliceString("abcdefghijklm", 4, 5));
     	vb.breakVigenere();
     }
     
